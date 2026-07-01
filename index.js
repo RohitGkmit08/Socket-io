a3644bc74ccb8956cad6fd13a6563054f0f54130
Rohit Sinha <rohitsinha0812@gmail.com>
2026-07-01 09:13:45 +0530

    Implement basic express socket.io server and index.html client

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-// 1. install packages
+import express from "express"
+import http, { createServer } from "http"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
+import { Server } from "socket.io"
 
-// 2. create instance
+const app = express();
+const server = createServer(app);
+// io = Socket.IO server object
+const io = new Server(server);
 
-// 3. serving html file
+const _dirname = dirname(fileURLToPath(import.meta.url));
 
-// 4. define a connection event handler 
+// serving an html file -> means sending an html file from server to browser in response to a request.
+app.get("/", (req, res) => {
+    res.sendFile(join(_dirname, "index.html"))
+})
 
-// 5. start the server 
+// defining a connection event handler  - io.on("connection", callback) is a method used to register event listner for different events that occur on server side.
+// io.on("connection", callback) -> whenever a new client is connected to Socket.io server, execute this callback.
+// "connection" is an event name built into Socket.io. Socket.IO emits this event whenever a browser successfully establishes a connection.
+io.on("connection", (socket) => {
+    console.log("user connected to server")
+})
+
+const PORT= 3000
+server.listen(PORT, ()=> {
+    console.log("server is running on port :", PORT)
+})
